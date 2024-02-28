3b42bdb86abeec796aea3e91e2b9648cca0899e5
Kolja <lasar@rasal.de>
2024-02-28 16:57:26 +0100

    ghpages-adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,25 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
-
+// import adapter from '@sveltejs/adapter-static';
+import adapterGhpages from "svelte-adapter-ghpages";
 // console.log('process.argv', process);
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: 'index.html'
-		}),
-
+		// adapter: adapter({
+		// 	pages: 'docs',
+		// 	assets: 'docs',
+		// 	fallback: 'index.html'
+		// }),
+		adapter: adapterGhpages({
+	    pages: 'build',
+      assets: 'build',
+      fallback: null
+		}
+    ),
 		paths: {
 			base: '/svelte-pen'
-			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
